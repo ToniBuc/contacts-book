87ed4b7b8281fb0f42ef4c8a0c0e08d19a33fa3a
Toni Buconjic <dyzzles@hotmail.com>
2024-07-29 12:38:03 +0200

    add dummy data to initial state and adjust change detection to OnPush

--- src/app/components/contact-details/contact-details.component.ts
+++ src/app/components/contact-details/contact-details.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { selectSelectedContact } from '../../state/contact.selectors';
 
 @Component({
   selector: 'app-contact-details',
   templateUrl: './contact-details.component.html',
-  styleUrl: './contact-details.component.scss'
+  styleUrl: './contact-details.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ContactDetailsComponent {
   public selectedContact$ = this.store.select(selectSelectedContact);
--- src/app/state/contact.reducer.ts
+++ src/app/state/contact.reducer.ts
@@ -8,7 +8,48 @@ export interface ContactState {
 }
 
 export const initialState: ContactState = {
-  contacts: [],
+  contacts: [
+    {
+        id: '1',
+        firstName: 'Toni',
+        lastName: 'Buconjic',
+        phone: '123456789',
+        email: 'placeholder1@placeholder.com',
+        address: 'random address 123'
+    },
+    {
+        id: '2',
+        firstName: 'John',
+        lastName: 'Smith',
+        phone: '987654321',
+        email: 'placeholder2@placeholder.com',
+        address: 'random address 456'
+    },
+    {
+        id: '3',
+        firstName: 'Jane',
+        lastName: 'Doe',
+        phone: '918273645',
+        email: 'placeholder3@placeholder.com',
+        address: 'random address 789'
+    },
+    {
+        id: '4',
+        firstName: 'Fourth',
+        lastName: 'Contact',
+        phone: '102938475',
+        email: 'placeholder4@placeholder.com',
+        address: 'random address 864'
+    },
+    {
+        id: '5',
+        firstName: 'Fifth',
+        lastName: 'Contact',
+        phone: '916424507',
+        email: 'placeholder5@placeholder.com',
+        address: 'random address 468'
+    },
+  ],
   selectedContact: null,
 };
 
